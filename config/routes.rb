b23582643f23926d00f9ac3f499530b801001a58
Kevin Forte <kevinforte9875@gmail.com>
2024-06-23 16:54:07 +0200

    File Added

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,20 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get 'home', to: 'home#home'
+  get 'about', to: 'about#about'
+  get 'contact_us', to: 'contact_us#contact_us'
+  get 'my_squad', to: 'my_squad#my_squad'
+
+  get 'leaderboard', to: 'leaderboard#leaderboard'
+  get 'insights', to: 'insights#insights'
+  get 'patch_notes', to: 'patch_notes#patch_notes'
+  get 'items', to: 'items#items'
+  get 'champion_ratings', to: 'champion_ratings#champion_ratings'
+  get 'squads', to: 'squads#squads'
+
+  get 'profile', to: 'profile#profile'
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
---
+++ test/controllers/my_squad_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class MySquadControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
