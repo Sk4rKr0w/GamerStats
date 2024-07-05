34af9ae64b32002fb7a854d04865019f15f39816
Kevin Forte <kevinforte9875@gmail.com>
2024-07-06 00:49:18 +0200

    Item Pop-up Added

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,7 @@
-# app/controllers/items_controller.rb
 class ItemsController < ApplicationController
   def index
     json_file_path = Rails.root.join('vendor', 'assets', 'dragontail-14.13.1', '14.13.1', 'data', 'en_US', 'item.json')
     @items = JSON.parse(File.read(json_file_path))["data"]
     @images = Dir.glob('app/assets/images/items/*')
-  end
-
-  def show
-    json_file_path = Rails.root.join('vendor', 'assets', 'dragontail-14.13.1', '14.13.1', 'data', 'en_US', 'item.json')
-    items = JSON.parse(File.read(json_file_path))["data"]
-    @item = items[params[:id]]
-    @image_path = Dir.glob("app/assets/images/items/*").find { |path| path.include?(params[:id]) }
   end
 end
