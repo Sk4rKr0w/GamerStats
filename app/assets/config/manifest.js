f31f7860be7442b4993e8d6caeb552c75b5a8a55
Kevin Forte <kevinforte9875@gmail.com>
2024-07-12 05:26:50 +0200

    My Squad Sketches CSS Uploaded

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,4 +2,5 @@
 //= link_directory ../stylesheets .css
 //= link_directory ../stylesheets/users .css
 //= link_directory ../stylesheets/devise .css
+//= link_directory ../stylesheets/squads/ .css
 //= link_tree ../../javascript .js
