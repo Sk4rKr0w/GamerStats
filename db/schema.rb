35da0d23e3b8ed2da46859e653bc3a972512b760
gabdif01 <153173399+gabdif01@users.noreply.github.com>
2024-07-13 19:21:11 +0200

    piccole modifiche

--- db/schema.rb
+++ db/schema.rb
@@ -97,6 +97,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_13_160135) do
     t.string "riot_tagline"
     t.string "continent"
     t.string "region"
+    t.boolean "two_factor_enabled"
+    t.datetime "last_two_factor_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
